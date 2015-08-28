10e3dce0b4ecc1dd309474ed0c7844131677f503
Thomas Menzel <menzelths@yahoo.de>
2015-08-28 15:00:08 +0200

    adoc unterstützung mit mathe und katex

--- tshare/src/main/java/de/qreator/tshare/Start.java
+++ tshare/src/main/java/de/qreator/tshare/Start.java
@@ -8,11 +8,7 @@ public class Start {
 		// TODO Auto-generated method stub
 		Vertx vertx = Vertx.vertx();
 		vertx.deployVerticle(VerticleClass.class.getName());
-<<<<<<< HEAD
-		System.out.println("Tesgt2324568");
-=======
-		System.out.println("Tesgt2324567");
->>>>>>> refs/remotes/origin/master
+
 
 	}
 
